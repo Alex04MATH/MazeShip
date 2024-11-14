8e8735a6c52cefcbc0400120cc4179f270f79740
Alex <alexmonterogaray@gmail.com>
2024-11-14 12:02:05 -0500

    feat: añadiendo primeras ideas sobre Clase Tablero y Celda

--- MazeShipLogic/Program.cs
+++ MazeShipLogic/Program.cs
@@ -1,12 +1,31 @@
 ﻿namespace MazeShipLogic
 {
    public class Tablero
-    {
-        public static void Get()
+   {
+       public int Dimension { get; }
+       public Celda[,] Tablero { get; }
+       public Tablero(int dimension)
+       {
+           Dimension=dimension;
+           Tablero=new Celda[Dimension,Dimension];
+           inicializarCelda();
+       }
+        private static void inicializarCelda()
         {
-            
+            for(int i=0; i<Dimension; i++)
+            for (int j = 0; j < Dimension; j++)
+            {
+                Tablero[i,j] = new Celda();
+            }
         }
     }
+
+   public class Celda
+   {
+       public bool EsObstaculo { get; set; }
+       public bool EsTrampa { get; set; }
+       
+   }
 
     public class Player
     {
